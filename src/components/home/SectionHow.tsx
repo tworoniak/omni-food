e1cd953a26a4b7469f1281aa2076b50832741d9f
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-10 21:49:45 -0600

    more content

--- src/components/home/SectionHow.tsx
+++ src/components/home/SectionHow.tsx
@@ -1,10 +1,16 @@
 const SectionHow = () => {
   return (
-    <section className='py-8 bg-shade grid grid-cols-2 gap-8'>
-      <div className='bg-red-500 p-8'>Test 1</div>
-      <div className='bg-blue-500 p-8'>Test 2</div>
-      <div className='bg-green-500 p-8'>Test 3</div>
-      <div className='bg-yellow-500 p-8'>Test 4</div>
+    <section className='py-8 bg-shade'>
+      <div className='container'>
+        <span>How It Works</span>
+        <h2>Your daily dose of health in 3 simple steps</h2>
+      </div>
+      <div className='container grid grid-cols-1 md:grid-cols-2 gap-8'>
+        <div className='bg-gray-900 p-8'>Test 1</div>
+        <div className='bg-gray-900 p-8'>Test 2</div>
+        <div className='bg-gray-900 p-8'>Test 3</div>
+        <div className='bg-gray-900 p-8'>Test 4</div>
+      </div>
     </section>
   );
 };
